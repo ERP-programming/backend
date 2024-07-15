d00ba8c42183e8b5fd521292bd6beb09b3751f99
retina3084 <106227154+retina3084@users.noreply.github.com>
2024-07-15 16:57:07 +0900

    Revert "- JSC커밋"
    This reverts commit c518c53887c22a66964c959db79d751677628bf5.

--- src/main/java/ac/su/erp/ErpApplication.java
+++ src/main/java/ac/su/erp/ErpApplication.java
@@ -2,10 +2,8 @@ package ac.su.erp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class ErpApplication {
 
     public static void main(String[] args) {
